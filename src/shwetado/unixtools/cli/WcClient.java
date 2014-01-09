c88eb0ecd096872ba594587e3065ed5d2373c77c
Shweta <shwetadongare28@gmail.com>
2014-01-09 10:02:09 +0530

    test cases added.

--- src/shwetado/unixtools/cli/WcClient.java
+++ src/shwetado/unixtools/cli/WcClient.java
@@ -3,21 +3,15 @@ package shwetado.unixtools.cli;
 import shwetado.fs.MyFileReader;
 import shwetado.unixtools.WordCount;
 
-public class WcClient {
-
+public class WcClient{
     WordCount wordCount;
-
     public WcClient(String fileName) {
         String text = new MyFileReader().readFile(fileName);
         this.wordCount = new WordCount(text);
     }
-
     public WordCount getWordCount() {
-
-
         return wordCount;
     }
-
     public static void main(String[] args) {
         WcClient wcClient = new WcClient(args[0]);
 
