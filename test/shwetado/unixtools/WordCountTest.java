f45ef9a23b6d651218a99f8716fac900cfe74d62
Shweta <shwetadongare28@gmail.com>
2014-01-09 10:04:53 +0530

    test cases added..

---
+++ test/shwetado/unixtools/WordCountTest.java
@@ -0,0 +1,27 @@
+package shwetado.unixtools;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WordCountTest {
+    @Test
+    public void testCountWords() throws Exception {
+        String text = "hello world";
+        WordCount wordCount = new WordCount(text);
+        Assert.assertEquals(1,wordCount.countWords());
+    }
+
+    @Test
+    public void testCountLines() throws Exception {
+        String text = "hello world";
+        WordCount wordCount = new WordCount(text);
+        Assert.assertEquals(1,wordCount.countLines());
+    }
+
+    @Test
+    public void testCountChars() throws Exception {
+        String text = "hello world";
+        WordCount wordCount = new WordCount(text);
+        Assert.assertEquals(11,wordCount.countChars());
+    }
+}
